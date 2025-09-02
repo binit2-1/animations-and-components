1463d23c64f7a829c1c1993d46d8c4c3c4a3af22
binit <binitgupta.1711@gmail.com>
2025-09-02 19:44:41 +0530

    added a duration randomiser

--- src/loaders/ZajnoLoader.jsx
+++ src/loaders/ZajnoLoader.jsx
@@ -7,6 +7,7 @@ gsap.registerPlugin(useGSAP, CustomEase);
 
 const ZajnoLoader = () => {
   useGSAP(() => {
+    // bezier curve randomization
     const stop1 = Math.random() * 0.2 + 0.15;
     const stop2 = stop1 + Math.random() * 0.2 + 0.15;
     const stop3 = stop2 + Math.random() * 0.2 + 0.15;
@@ -14,6 +15,9 @@ const ZajnoLoader = () => {
     const firstStop = Math.min(stop1, 0.4);
     const secondStop = Math.min(stop2, 0.7);
     const thirdStop = Math.min(stop3, 0.9);
+
+    //duration randomiser
+    const duration = Math.random() * 4 + 4; // Random duration between 4 and 8 seconds
     
     const dynamicEase = `M0,0 L0.3,${firstStop} L0.35,${firstStop} L0.6,${secondStop} L0.65,${secondStop} L0.85,${thirdStop} L0.9,${thirdStop} L1,1`;
     
@@ -22,7 +26,7 @@ const ZajnoLoader = () => {
     gsap.to('.zajno-black', {
       textContent: 100,
       snap: { textContent: 1 },
-      duration: 8,
+      duration: duration,
       ease: "dynamicEase",
     });
   });
