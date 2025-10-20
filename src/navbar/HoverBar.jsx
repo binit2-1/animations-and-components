7b1df78755ded1ca7cf545093a7d2cc9ae357e50
binit <binitgupta.1711@gmail.com>
2025-10-21 01:37:24 +0530

    used gsap to animate

--- src/navbar/HoverBar.jsx
+++ src/navbar/HoverBar.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
 
 const HoverBar = () => {
+  const handleHoverEnter = () => {
+    gsap.from(".navbar",{
+      width:"480px",
+      height:"60px",
+      duration:0.5,
+      ease:"power2.out"
+    })
+    gsap.to(".navbar",{
+      width:"540px",
+      height:"880px",
+      duration:0.5,
+      ease:"power2.out"
+    })
+  }
+  const handleHoverLeave = () => {
+    gsap.from(".navbar",{
+      width:"540px",
+      height:"880px",
+      duration:0.5,
+      ease:"power2.out"
+    })
+    gsap.to(".navbar",{
+      width:"480px",
+      height:"60px",
+      duration:0.5,
+      ease:"power2.out"
+    })
+
+  }
+
   return (
-    <div className="sticky w-ful h-full">
-      <div className="flex justify-center w-full h-full ">
-        <div className="flex justify-flex items-flex px-4 py-3.5 text-white font-[hover-bar] text-3xl bg-liquid-glass w-120 h-15 hover:w-135 hover:h-220 transition-all ease-in-out duration-1000 mt-5 rounded-2xl">
+    <div className="sticky w-full h-full">
+      <div className="flex justify-center w-full h-full">
+        <div onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverLeave} className="navbar absolute flex px-4 py-3.5 text-white font-[hover-bar] text-3xl bg-liquid-glass w-120 h-15 mt-5 rounded-2xl">
           Hover Me
         </div>
       </div>
